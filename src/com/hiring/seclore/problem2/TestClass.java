1c0450ba931aeb304252c6d64bd4c8bd70e42fb7
Uday Kumar <u.uday.k.kumar@oracle.com>
2018-11-18 20:36:03 +0530

    adding problems for hiring challenge of Seclore

--- src/com/hiring/seclore/problem2/TestClass.java
+++ src/com/hiring/seclore/problem2/TestClass.java
@@ -1,4 +1,4 @@
-package com.hiring.seclore.problem1;
+package com.hiring.seclore.problem2;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
@@ -6,21 +6,17 @@ import java.io.InputStreamReader;
 class TestClass {
 	public static void main(String args[]) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String name = br.readLine(); // Reading input from STDIN
-		int N = Integer.parseInt(name);
-
-		int[] arrA = new int[N];
-		int[] arrB = new int[N];
+		String input = br.readLine(); // Reading input
+		int N = Integer.parseInt(input);
 
 		String[] A = br.readLine().split(" ");
 		String[] B = br.readLine().split(" ");
+		int[] arrA = new int[N];
+		int[] arrB = new int[N];
 		for (int i = 0; i < N; i++) {
 			arrA[i] = Integer.parseInt(A[i]);
 			arrB[i] = Integer.parseInt(B[i]);
 		}
-		int M = Integer.parseInt(br.readLine());
-		System.out.println(M);
 
 	}
-
 }
