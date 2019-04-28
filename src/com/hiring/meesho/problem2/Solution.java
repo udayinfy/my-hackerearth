b12c30a4d72399e7a2ddfcad51360c77d2066904
Uday Kumar <u.uday.k.kumar@oracle.com>
2019-04-29 00:59:11 +0530

    meesho hiring challenge 2019

---
+++ src/com/hiring/meesho/problem2/Solution.java
@@ -0,0 +1,26 @@
+package com.hiring.meesho.problem2;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Solution {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String name = br.readLine(); // Reading input from STDIN
+		int N = Integer.parseInt(name);
+
+		int[] arrA = new int[N];
+		int[] arrB = new int[N];
+
+		String[] A = br.readLine().split(" ");
+		String[] B = br.readLine().split(" ");
+		for (int i = 0; i < N; i++) {
+			arrA[i] = Integer.parseInt(A[i]);
+			arrB[i] = Integer.parseInt(B[i]);
+		}
+		int M = Integer.parseInt(br.readLine());
+		System.out.println(M);
+
+	}
+
+}
